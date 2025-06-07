ba57c37910cbe9c8a4b56173714496c714ba766f
pde0814 <dongeon8943@gmail.com>
2025-06-08 06:25:54 +0900

    chore: 초기 폴더 구조 및 스토리보드 분리

---
+++ LeaveGo/Feature/Planner/PlannerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlannerViewController.swift
+//  LeaveGo
+//
+//  Created by 박동언 on 6/8/25.
+//
+
+import UIKit
+
+class PlannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
