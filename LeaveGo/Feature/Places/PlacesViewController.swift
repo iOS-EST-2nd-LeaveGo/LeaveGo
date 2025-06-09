189fec0c7173cfd2fcb8b458d3d5299d22f936eb
YunCheolJeon <perfecctism@gmail.com>
2025-06-09 11:46:44 +0900

    initial commit xib 구현

--- LeaveGo/Feature/Places/PlacesViewController.swift
+++ LeaveGo/Feature/Places/PlacesViewController.swift
@@ -7,23 +7,42 @@
 
 import UIKit
 
-class PlacesViewController: UIViewController {
-
+class PlacesViewController: UIViewController, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let nib = UINib(nibName: "ListTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "ListTableViewCell")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
+  
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension PlacesViewController: UITableViewDataSource {
+    // 셀 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 // 임시로 10개. 나중엔 API에서 받아온 데이터 개수로 변경
     }
-    */
 
+    // 셀 구성
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListTableViewCell", for: indexPath) as? ListTableViewCell else {
+            return UITableViewCell()
+        }
+
+        // 셀 구성 예시
+        cell.thumbnailImageView.image = UIImage(systemName: "photo")
+        cell.distanceLabel.text = "거리 표시"
+        cell.titleLabel.text = "장소 제목 \(indexPath.row + 1)"
+        cell.timeLabel.text = "오픈 시간 등"
+
+        return cell
+    }
 }
