fd85680882e691d4011ce39dcc870111ddd838a5
LeeChiHoon <dlclgns152615@gmail.com>
2025-06-09 16:04:06 +0900

    [feat] MapViewController 사용자 위치 annotation 추가 및 지도 포커싱 기능 추가

--- LeaveGo/Feature/Places/MapViewController.swift
+++ LeaveGo/Feature/Places/MapViewController.swift
@@ -21,17 +21,56 @@ class MapViewController: UIViewController {
     locationManager.requestWhenInUseAuthorization()
     return locationManager
   }()
+  
+  // UI
   var mapView: MKMapView!
+  var userLocationButton: UIButton = {
+      let button = UIButton()
+      button.backgroundColor = .white
+      button.layer.cornerRadius = 20
+      button.layer.shadowColor = UIColor.black.cgColor
+      button.layer.shadowOffset = CGSize(width: 0, height: 2)
+      button.layer.shadowOpacity = 0.5
+      button.layer.shadowRadius = 3
+      return button
+    }()
+  let userLocationImageView = UIImageView(image: UIImage(named: "btn_location"))
   
   // MARK: LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    mapView = MKMapView(frame: view.frame)
+    
     
     setupCLLocationManager()
     setupMapView()
     configureSubviews()
+    addTarget()
   }
+  
+  func addTarget() {
+      userLocationButton.addTarget(self, action: #selector(setMapRegion), for: .touchUpInside)
+    }
+  
+  // MARK: - Action
+    
+    @objc func setMapRegion() {
+      self.userLocationImageView.image = self.userLocationImageView.image?.withTintColor(.systemBlue)
+      
+      var coordiCenterLa = mapView.userLocation.coordinate.latitude
+      let coordiCenterLo = mapView.userLocation.coordinate.longitude
+//      coordiCenterLa -= 0.001
+      let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: coordiCenterLa, longitude: coordiCenterLo),
+                                      latitudinalMeters: 450, longitudinalMeters: 450)
+      mapView.setRegion(region, animated: true)
+//      bottomSheetView.mode = .tip // sheetview dismiss
+      
+     // bottomSheetView.hiddenDetailView()
+//      mapView.removeMapViewOverlayOfLast() // 화면에 나타난 경로 삭제
+     
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) { // 삭제하는게 좋겠음 아니면 completion 처리하든가
+        self.userLocationImageView.image = self.userLocationImageView.image?.withTintColor(.black)
+      }
+    }
   
 }
 
@@ -39,6 +78,8 @@ class MapViewController: UIViewController {
 extension MapViewController: MKMapViewDelegate {
   
   func setupMapView() {
+    mapView = MKMapView(frame: view.frame)
+    
     mapView.delegate = self
     mapView.showsUserLocation = true // 사용자 위치
   }
@@ -57,6 +98,55 @@ extension MapViewController: MKMapViewDelegate {
       mapView.setRegion(region, animated: true)
     }
   }
+  
+  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+      // 사용자 현재위치의 view setting
+//      if annotation is MKUserLocation {
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "userlocation")
+        annotationView.image = UIImage(named: "img_userlocation")
+    annotationView.layer.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        annotationView.layer.shadowColor = UIColor.orange.cgColor
+        annotationView.layer.shadowOffset = CGSize(width: 1, height: 1)
+        annotationView.layer.shadowOpacity = 0.5
+        annotationView.layer.shadowRadius = 5
+        // ios 16 이상부터는 layer없이 바로 anchorpoint를 설정할 수 있음!
+        return annotationView
+//      }
+      
+//      guard let annotation = annotation as? TrashAnnotation else {
+//        return nil
+//      }
+      
+//      var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier:
+//                                                                  TrashAnnotationView.identifier)
+//      
+//      if annotationView == nil {
+//        annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier:
+//                                            TrashAnnotationView.identifier)
+//        annotationView?.canShowCallout = false
+//        annotationView?.contentMode = .scaleAspectFit
+//      } else {
+//        annotationView?.annotation = annotation
+//      }
+//      
+//      let annotationImage: UIImage!
+//      let size = CGSize(width: 40, height: 40)
+//      UIGraphicsBeginImageContext(size)
+//      
+//      switch annotation.imageType {
+//      case 0:
+//        annotationImage = UIImage(named: "TrashPin")
+//      case 1:
+//        annotationImage = UIImage(named: "StorePin")
+//      default:
+//        annotationImage = UIImage(systemName: "trash.circle")
+//      }
+//      annotationImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+//      let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+//      annotationView?.image = resizedImage
+//      
+//      return annotationView
+    }
   
   /// 경로위에 표시되는 line UI를 정의
   func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
@@ -109,11 +199,27 @@ extension MapViewController: LayoutSupport {
   
   func addSubviews() {
     self.view.addSubview(mapView)
+    mapView.addSubview(userLocationButton)
     
+    userLocationButton.addSubview(userLocationImageView)
   }
   
   func setupSubviewsConstraints() {
-    print("")
+    userLocationButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      userLocationButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+      userLocationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+      userLocationButton.heightAnchor.constraint(equalToConstant: 40),
+      userLocationButton.widthAnchor.constraint(equalToConstant: 40)
+      ])
+    
+    userLocationImageView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      userLocationImageView.topAnchor.constraint(equalTo: userLocationButton.topAnchor, constant: 8),
+          userLocationImageView.bottomAnchor.constraint(equalTo: userLocationButton.bottomAnchor, constant: -8),
+          userLocationImageView.leadingAnchor.constraint(equalTo: userLocationButton.leadingAnchor, constant: 8),
+          userLocationImageView.trailingAnchor.constraint(equalTo: userLocationButton.trailingAnchor, constant: -8)
+    ])
   }
   
 }
