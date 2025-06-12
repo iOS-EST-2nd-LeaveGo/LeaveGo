40873f80b5f486d5c8917dee1880a95db29c74f7
LeeChiHoon <dlclgns152615@gmail.com>
2025-06-12 16:39:56 +0900

    [feat] PlaceModel 생성

---
+++ LeaveGo/Feature/Places/Model/PlaceModel.swift
@@ -0,0 +1,26 @@
+//
+//  PlaceModel.swift
+//  LeaveGo
+//
+//  Created by 이치훈 on 6/12/25.
+//
+
+import Foundation
+
+struct PlaceModel {
+    let contentId: String // 장소 고유번호
+    let title: String // use in PlacesVC
+    let thumbnailImage: String? // use in PlacesVC
+    let distance: String // use in PlacesVC
+    let latitude: Double // use in PlacesVC
+    let longitude: Double // use in PlacesVC
+    
+    let detail: PlaceDetailModel?
+}
+
+struct PlaceDetailModel {
+    let contentId: String // 장소 고유번호
+    let contentTypeId: String // 장소 관광지 타입
+    let infoCenter: String? // 안내센터명 또는 전화번호
+    let openTime: String? // 운영시간
+}
