ceac71dfd20bd2b676b6bde515d75b4491306326
kitcat-s <kitcat92.seo@gmail.com>
2025-06-12 16:52:41 +0900

    feat: 장소 목록 셀 재사용을 위한 셀 모드 열거형 생성

---
+++ LeaveGo/Feature/Places/Model/CellMode.swift
@@ -0,0 +1,14 @@
+//
+//  CellMode.swift
+//  LeaveGo
+//
+//  Created by Kitcat Seo on 6/12/25.
+//
+
+import Foundation
+
+enum CellMode {
+    case list
+    case selectable
+    case draggable
+}
