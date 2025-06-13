89877d460da954fe86564be8ce1314c7b2dacddf
pde0814 <dongeon8943@gmail.com>
2025-06-13 21:23:30 +0900

    fix: cell UI 제약변경 및 morebutton 변경

--- LeaveGo/Feature/Places/PlacesViewController.swift
+++ LeaveGo/Feature/Places/PlacesViewController.swift
@@ -63,19 +63,6 @@ extension PlacesViewController: UITableViewDataSource {
         // 간단한 시간 정보 (추후 detailIntro2 API로 대체 가능)
         cell.timeLabel.text = "09:00 ~ 18:00 • 1시간"
         
-        cell.moreButtonTapped = {
-            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-            actionSheet.addAction(UIAlertAction(title: "오프라인 저장하기(샘플)", style: .default))
-            actionSheet.addAction(UIAlertAction(title: "경로 찾기(샘플)", style: .default))
-            actionSheet.addAction(UIAlertAction(title: "취소", style: .cancel))
-            
-            if let popover = actionSheet.popoverPresentationController {
-                popover.sourceView = cell
-                popover.sourceRect = cell.bounds
-            }
-            
-            self.present(actionSheet, animated: true)
-        }
         
         // 이미지 처리
         // 1. PlaceList의 thumbnailImage는 String? 타입이므로 nil인지 확인
