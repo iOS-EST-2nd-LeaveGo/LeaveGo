2811b713018e20db037984ce3d4406b23b74425d
kitcat-s <kitcat92.seo@gmail.com>
2025-06-13 18:20:09 +0900

    Design: 여행 상세 페이지 작업 시작

---
+++ LeaveGo/Feature/Planner/PlaceSelectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlaceSelectionViewController.swift
+//  LeaveGo
+//
+//  Created by Kitcat Seo on 6/13/25.
+//
+
+import UIKit
+
+class PlaceSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
