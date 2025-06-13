dbc4e52cd97298ca947eb0cde1ee93fc512749cc
pde0814 <dongeon8943@gmail.com>
2025-06-13 10:10:18 +0900

    feat: CoreData 기본 세팅

---
+++ LeaveGo/Shared/Persistence/Coredata/CoreDataManager+ScheduleItem.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataManager+ScheduleItem.swift
+//  LeaveGo
+//
+//  Created by 박동언 on 6/13/25.
+//
+
+import Foundation
+import CoreData
+
+extension CoreDataManager {
+
+    // func createScheduleItem
+    // func fetchScheduleItems
+    // func updateScheduleItem
+    // func deleteScheduleItem
+
+}
