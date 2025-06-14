12abeed68b22aa8e5dbb113bb72f1a31f6dc7838
김효환 <gyghks1017@gmail.com>
2025-06-14 23:57:05 +0900

    fix : 충돌 해결

--- LeaveGo/Application/SceneDelegate.swift
+++ LeaveGo/Application/SceneDelegate.swift
@@ -15,7 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-<<<<<<< HEAD
         
         
 //        guard let _ = (scene as? UIWindowScene) else { return }
@@ -47,22 +46,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     } else {
                         print("❗️ targetVC 못 찾음. Storyboard ID 다시 확인!")
                     }
-=======
-        guard let _ = (scene as? UIWindowScene) else { return }
-		// MARK: 기본코드(공통코드)
-        let didFinishOnboarding = UserDefaults.standard.bool(forKey: "didFinishOnboarding")
-        if didFinishOnboarding {
-			let mainSB = UIStoryboard(name: "Main", bundle: nil)
-            let mainTabBar = mainSB.instantiateViewController(withIdentifier: "MainTabBarController")
-            window?.rootViewController = mainTabBar
-        } else {
-            let onboardingSB = UIStoryboard(name: "Onboarding", bundle: nil)
-            let onboardingNav = onboardingSB.instantiateViewController(withIdentifier: "OnboardingNav")
-            window?.rootViewController = onboardingNav
-        }
 
-		        window?.makeKeyAndVisible()
->>>>>>> ddc770a68e8a888175d3d2301d1f10096d461025
     }
 
 
