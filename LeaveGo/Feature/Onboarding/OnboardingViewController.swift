a990af91499f5d236cba503b9fa9c2465ab4ce28
pde0814 <dongeon8943@gmail.com>
2025-06-18 03:47:17 +0900

    feat: 온보딩뷰 소개 페이지 세팅 완료

--- LeaveGo/Feature/Onboarding/OnboardingViewController.swift
+++ LeaveGo/Feature/Onboarding/OnboardingViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 // 온보딩 소개화면 추후구현
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     @IBOutlet weak var saveButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        saveButton.isEnabled = false
+
         navigationItem.backButtonTitle = ""
 
-        saveButton.layer.cornerRadius = saveButton.frame.height / 2
+        saveButton.layer.cornerRadius = 16
         saveButton.clipsToBounds = true
     }
+}
 
+extension OnboardingViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x / scrollView.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+
+        if pageControl.currentPage == 2 {
+            saveButton.isEnabled = true
+        }
+    }
 }
