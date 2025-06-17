a9e02ebfc0313f53c93f4f59c53e407713d5fe38
kitcat-s <kitcat92.seo@gmail.com>
2025-06-17 16:37:07 +0900

    feat: 더미 위치를 기반으로 근처 여행지 불러오는 작업 중

--- LeaveGo/Feature/Home/HomeViewController.swift
+++ LeaveGo/Feature/Home/HomeViewController.swift
@@ -9,8 +9,73 @@ import UIKit
 import CoreLocation
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var welcomMessageLabel: UILabel!
+    @IBOutlet weak var placeCollectionView: UICollectionView!
+    
+    var placeList: [PlaceList]?
+    
+    private var currentLocation: CLLocationCoordinate2D?
+    private var hasLoadedPlaceList = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let userName = UserDefaults().string(forKey: "nickname") {
+            welcomMessageLabel.text = "\(userName)님이 좋아하실만한\n주변 관광지들을 골라봤어요."
+        } else {
+            welcomMessageLabel.text = "내 주변 추천 관광지"
+        }
+        
+        placeCollectionView.delegate = self
+        placeCollectionView.dataSource = self
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // LocationManager 작동 방식을 몰라서 현재 위치 강제 지정
+        currentLocation = CLLocationCoordinate2D(latitude: 126.76857234333737, longitude: 37.51006358933778)
+        
+        loadPlaceList()
+    }
+    
+    private func loadPlaceList() {
+        Task {
+            guard let currentLocation else {
+                print("위치 없음")
+                return
+            }
+            
+            placeList = try await NetworkManager.shared.fetchPlaceList(
+                numOfRows: 5,
+                mapX: currentLocation.latitude,
+                mapY: currentLocation.longitude,
+                radius: 2000)
+            
+            await MainActor.run {
+                updateCollectionView()
+            }
+        }
+    }
+    
+    private func updateCollectionView() {
+        
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return placeList?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecommendedPlaceCardCell", for: indexPath)
+        return cell
+    }
+    
+    
 }
