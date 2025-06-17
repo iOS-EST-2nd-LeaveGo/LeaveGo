f9240433eecd8802e850ee27da3ab34a5eb667d8
김효환 <gyghks1017@gmail.com>
2025-06-17 15:22:23 +0900

    feat : 버튼 추가

--- LeaveGo/Feature/NewTrip/PlannerEditorViewController.swift
+++ LeaveGo/Feature/NewTrip/PlannerEditorViewController.swift
@@ -38,8 +38,7 @@ class PlannerEditorViewController: UIViewController {
         tripListTableView.dropDelegate = self
 
         // ✅ 임시 데이터 추가
-        placeList = [
-            PlaceModel(
+        placeList = [PlaceModel(
                 add1: "서울특별시 송파구",
                 add2: "신천동",
                 contentId: "1001",
@@ -50,21 +49,7 @@ class PlannerEditorViewController: UIViewController {
                 longitude: "127.100",
                 areaCode: "1",
                 cat1: "A", cat2: "B", cat3: "C"
-            ),
-            PlaceModel(
-                add1: "서울특별시 송파구",
-                add2: "잠실동",
-                contentId: "1002",
-                title: "잠실 롯데타워",
-                thumbnailURL: nil,
-                distance: "2.5km",
-                latitude: "37.513",
-                longitude: "127.102",
-                areaCode: "2",
-                cat1: "A", cat2: "C", cat3: "D"
-            )
-        ]
-
+            )]
     }
 
     // 썸네일 사진 선택 / 삭제 버튼 토글
@@ -176,4 +161,6 @@ extension PlannerEditorViewController: UITableViewDragDelegate, UITableViewDropD
             }
         }
     }
+    
+    
 }
