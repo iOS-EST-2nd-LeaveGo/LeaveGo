a03c9e5a416a9d148ac24b250e2676e4e7e4c2e3
Kim Seohyun <46340978+cestbonciel@users.noreply.github.com>
2025-06-19 07:12:12 +0900

    온보딩 이미지랑 설명 추가 (#103)
    * Design: 스토리보드 온보딩 사진, 설명 설정
    
    * Design: 온보딩 페이저 오류 수정
    
    * Delete LeaveGo/Secrets.plist
    
    ---------
    
    Co-authored-by: kitcat <64347093+kitcat-s@users.noreply.github.com>

--- LeaveGo/Feature/Onboarding/OnboardingViewController.swift
+++ LeaveGo/Feature/Onboarding/OnboardingViewController.swift
@@ -13,27 +13,34 @@ class OnboardingViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     
-    @IBOutlet weak var saveButton: UIButton!
+	@IBOutlet weak var stackView: UIStackView!
+	@IBOutlet weak var saveButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        saveButton.isEnabled = false
-
-        navigationItem.backButtonTitle = ""
-
-        saveButton.layer.cornerRadius = 16
-        saveButton.clipsToBounds = true
+		let totalPages = stackView.arrangedSubviews.count
+		pageControl.numberOfPages = totalPages
+		pageControl.currentPage = 0
+		
+		// 2) 페이징 활성화 & 델리게이트 연결
+		scrollView.isPagingEnabled = true
+		scrollView.delegate = self
+		
+		// 3) 버튼 초기 스타일
+		saveButton.isEnabled = false
+		saveButton.layer.cornerRadius = 16
+		saveButton.clipsToBounds = true
+		
+		navigationItem.backButtonTitle = ""
     }
 }
 
 extension OnboardingViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let pageIndex = round(scrollView.contentOffset.x / scrollView.frame.width)
-        pageControl.currentPage = Int(pageIndex)
+		let page = Int(scrollView.contentOffset.x / scrollView.frame.width)
+		pageControl.currentPage = page
 
-        if pageControl.currentPage == 2 {
-            saveButton.isEnabled = true
-        }
+		saveButton.isEnabled = (page == pageControl.numberOfPages - 1)
     }
 }
 
